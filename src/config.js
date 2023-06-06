dda2fe39360ce4e8ccff7825308862484fee0745
didava <farshad.javan@gmail.com>
2023-06-06 15:54:14 +0330

    v1.0.0

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  port: 9000,
-  limit: 1,
-  timer: 30000,
-  verbose: true
+  port: 9000, // REST API port
+  limit: 1, // Counter limit for auto save functionality
+  timer: 30000, // Interval for auto save depending on counter limit
+  verbose: true // Show action logs on console
 }
